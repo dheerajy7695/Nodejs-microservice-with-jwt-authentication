6b6d23af7a44ea6424a806efe91acd225966bb95
dheerajy7695 <dheerajy906@gmail.com>
2025-01-26 13:42:35 +0530

    Minor change in produc-service

--- product-service.js
+++ product-service.js
@@ -5,8 +5,8 @@ const port = process.env.port || 3002;
 
 app.get('/products', (req, res) => {
     res.json([
-        { id: 1001, quantity: 2, product: "Shooes" },
-        { id: 1002, quantity: 20, product: "Shorts" }
+        { id: 1001, quantity: 2, product: "Shooes", size: [5, 6, 7] },
+        { id: 1002, quantity: 20, product: "Shorts", size: [S, M, L, XL] }
     ])
 });
 
