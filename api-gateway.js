b72c701c879476ffd87d5c2656ea575c6de90112
dheerajy7695 <dheerajy906@gmail.com>
2025-01-26 13:34:02 +0530

    Minor changes

--- api-gateway.js
+++ api-gateway.js
@@ -29,7 +29,7 @@ function authValidation(req, res, next) {
 function authRole(role) {
     return (req, res, next) => {
         if (req.user.role !== role) {
-            return res.status(403).json('Unauthorized')
+            return res.status(403).json('Unauthorized');
         }
         next();
     }
